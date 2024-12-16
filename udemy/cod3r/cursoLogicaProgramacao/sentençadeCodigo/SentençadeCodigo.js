dd8ac28d823c5f2cb6355884e3eae26f3f162906
maximos34 <44528369+maximos34@users.noreply.github.com>
2024-12-16 20:04:51 -0300

    começando curso do luiz otavio
    assistindo tbm as aulas do professor luiz otavio para estudos

--- udemy/cod3r/cursoLogicaProgramacao/sentençadeCodigo/SentençadeCodigo.js
+++ udemy/cod3r/cursoLogicaProgramacao/sentençadeCodigo/SentençadeCodigo.js
@@ -3,3 +3,13 @@
 const form = function (){
     console.log('escreva aqui sua sentença de codigo')
 };
+
+/* uma senteça de codigo pode ocupar varias linhas  */
+
+console
+.log(
+    'esse é um bloco de codigo'
+);
+
+/* só n pode quebrar dentro das aspas o sistema ira da erro  e tbm vc pode por varios espaços em bora n fique bonito para quem for ler o codigo */
+
