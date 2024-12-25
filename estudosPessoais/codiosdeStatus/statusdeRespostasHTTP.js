18c714526411af36c041c73910eff3d99b31e9ae
maximos34 <44528369+maximos34@users.noreply.github.com>
2024-12-25 14:13:41 -0300

    aulas de tipos primitivos

---
+++ estudosPessoais/codiosdeStatus/statusdeRespostasHTTP.js
@@ -0,0 +1,16 @@
+/*
+*CODIGOS DE STATUS DE RESPOSTAS HTTP
+
+* os codigos de status de respostas HTTP indicam se uma solicitação HTTP especifica foi concluida com êxito .
+* As respostas são agrupadas em cinco classes 
+*/
+
+//.... 1°Respostas informativas (100 --- 199)
+//.... 2°Respostas Bem-sucedidas (200---299)
+//.... 3°Respostas de redirecionamento (300---399)
+//.... 4°Respostas de erro do cliente (400---499)
+//.... 5°Respostas de erro do servidor (500---599)  
+
+//.... os codigos de status listados abaixo são definidos por RFC9110 
+
+
---
+++ udemy/luizOtavio/cursoJavaScripteType/javascriptBasico/aulas/tiposPrimitivos.js
@@ -0,0 +1,28 @@
+/*
+* aula de tipos primitivos em javascript
+
+.... um valor primitivo é um dado que não é representado através de um Objeto e , por consequencia, não possui metodos 
+
+>< na maior parte do tempo,um valor primitivo é representado diretamente através do mais baixo nivel de implementação de uma linguagem 
+
+! no js, existem 6 tipos primitivos que são :
+
+*string
+*number
+*boolean
+*null 
+*undefined
+*symbol(novo no ecmascript 6)
+
+
+.... string : são cadeias de caracteres e são delimitados pelas as aspas tantos simples como aspas duplas , alem das crases usadas nas templates strings 
+
+* const nome = 'Pedro';
+*const nome = "Pedro";
+*const nome = `Pedro`;
+
+
+
+
+
+*/
